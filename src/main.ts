7043904a41b7575b57e936e3f07c7092f1dca3f1
Kevin McGarry <kevin.mcgarry@over-c.com>
2025-11-13 12:39:38 +0000

    trying to make analytics build

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,4 @@ import { useTheme } from "./composables/useTheme";
 
 useTheme();
 
-if (import.meta.env.PROD) {
-  import("@vercel/analytics")
-    .then(({ inject }) => inject())
-    .catch((error) => {
-      console.error("Failed to initialize Vercel Analytics", error);
-    });
-
-  import("@vercel/speed-insights")
-    .then(({ injectSpeedInsights }) => injectSpeedInsights())
-    .catch((error) => {
-      console.error("Failed to initialize Vercel Speed Insights", error);
-    });
-}
-
 createApp(App).mount("#app");
