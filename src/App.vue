6955d966b0c3924f8fc3d5d35dc0cdeed81f6c44
Kevin McGarry <kevin.mcgarry@over-c.com>
2025-11-13 12:25:49 +0000

    propely added alanytics and speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { Dashboard, GlobalNoticeStack } from "./components";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <SpeedInsights />
   <Dashboard />
   <GlobalNoticeStack />
+  <Analytics />
 </template>
 
 <style scoped></style>
