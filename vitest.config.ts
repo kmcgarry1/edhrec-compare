de45c451bf16e0f321680a0ac3a96830d50d60ed
Kevin McGarry <kevin.mcgarry@over-c.com>
2025-11-20 17:12:11 +0000

    updated ignore list targeting components that are prime targets for refactoring

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,16 @@ export default defineConfig({
     coverage: {
       reporter: ["text", "html", "json-summary"],
       reportsDirectory: "coverage/unit",
+      exclude: [
+        "src/components/Dashboard.vue",
+        "src/components/CommanderDisplay.vue",
+        "src/components/DropdownSelect.vue",
+        "src/components/EdhrecReader.vue",
+        "src/components/ScryfallCardRow.vue",
+        "src/components/SiteNotice.vue",
+        "src/components/index.ts",
+        "src/components/core/index.ts",
+      ],
     },
   },
 });
